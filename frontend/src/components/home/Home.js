d777ebaf7e348389e07f4a797425d2ca6758c65e
Harikrishnan K <harikdas2001@gmail.com>
2023-08-20 16:44:10 +0530

    sections

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -2,7 +2,7 @@ import Welcome from "../welcome/Welcome";
 import Navbar from "../navbar/Navbar";
 function Home() {
   return (
-    <div>
+    <div style={{ overflow: 'hidden' }}>
         <Navbar/>
         <Welcome/>
         {/* <Footer/> */}
--- frontend/src/components/welcome/Welcome.js
+++ frontend/src/components/welcome/Welcome.js
@@ -3,34 +3,53 @@ import Logo from "../../assets/images/logo.png";
 function Welcome() {
     return (
         <div className="welcome">
-        <div className="hero">
-            <img src={Logo} className="hero-logo"></img>
-            <HeroBox/>
-        </div>
+            <div className="hero">
+                <img src={Logo} className="hero-logo"></img>
+                <HeroBox />
 
-        {/* <div>
-            aaaaaaaaaaaaaaaaaaaasssssssssssssssssdddddddddddd
-        </div> */}
-        
+                <button className="join-button" >
+                    Join
+                </button>
+            </div>
+
+            <div className="welcome-section">
+                <div className="section-title">
+                    <div className="section-title-bullet"></div>
+                    <span className="section-title-text">Section 1...............</span>
+                </div>
+                <div>
+                    Content
+                </div>
+            </div>
+
+
+            {/* <div className="statistics">
+                aaaaaaaaaaaaaaaaaaaasssssssssssssssssdddddddddddd
+            </div>
+
+            <div className="testimonials">
+                aaaaaaaaaaaaaaaaaaaasssssssssssssssssdddddddddddd
+            </div> */}
+
         </div>
     );
-  }
-  
+}
 
-function HeroBox(){
-return(
-    <>
-    <div className="hero-box">
-        <span style={{ fontSize: '38px'}}>
-            "Roommates are there for more than just splitting the rent.
-            They're there for the random, unexpected
-            moments that make life unforgettable.” 
-            </span>
-            <span style={{ fontSize: '38px',color:'#210203'  }}> - Bubble </span>
-    </div>
-    
-    </>
-)
+
+function HeroBox() {
+    return (
+        <>
+            <div className="hero-box">
+                <span style={{ fontSize: '38px' }}>
+                    "Roommates are there for more than just splitting the rent.
+                    They're there for the random, unexpected
+                    moments that make life unforgettable.”
+                </span>
+                <span style={{ fontSize: '38px', color: '#210203' }}> - Bubble </span>
+            </div>
+
+        </>
+    )
 }
 
 export default Welcome;
