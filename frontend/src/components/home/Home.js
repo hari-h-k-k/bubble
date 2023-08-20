53fcf20b41ca75be107e33edd3bf1bdc5fce1850
Harikrishnan K <harikdas2001@gmail.com>
2023-08-20 19:14:20 +0530

    testimonials

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,12 +1,13 @@
 import Welcome from "../welcome/Welcome";
 import Navbar from "../navbar/Navbar";
+import Footer from "../footer/Footer";
 
 function Home() {
     return (
         <>
             <Navbar/>
             <Welcome/>
-            {/* <Footer/> */}
+            <Footer/>
         </>
     );
 }
--- frontend/src/components/welcome/Welcome.js
+++ frontend/src/components/welcome/Welcome.js
@@ -1,5 +1,10 @@
 import "./Welcome.css";
 import Logo from "../../assets/images/logo.png";
+import Bubbles from "../../assets/images/bubbles.png";
+import User from "../../assets/images/user.png";
+import Globe from "../../assets/images/globe.png";
+import Avatar from "../../assets/images/avatar.png";
+
 function Welcome() {
     return (
         <>
@@ -15,12 +20,49 @@ function Welcome() {
             <div className="welcome-section">
                 <div className="section-title">
                     <div className="section-title-bullet"></div>
-                    <span className="section-title-text">Section 1...............</span>
+                    <span className="section-title-text">Expand Your Horizons, Meet New People</span>
                 </div>
-                <div>
-                    Content
+                <div className="section-content">
                 </div>
             </div>
+
+
+            <div className="welcome-section">
+                <div className="section-title">
+                    <div className="section-title-bullet" style={{ backgroundColor: "#D9D9D9" }}></div>
+                    <span className="section-title-text">Rapid Connections, Limitless Possibilities</span>
+                </div>
+                <div className="section-content" style={{ backgroundColor: "#D9D9D9", minHeight: "200px", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+                    <div style={{ textAlign: "center" }}>
+                        <img src={Globe} style={{ height: "100px", width: "100px" }}></img>
+                        <div style={{ fontSize: "25px" }}>0+ Countries</div>
+                    </div>
+
+                    <div style={{ textAlign: "center" }}>
+                        <img src={Bubbles} style={{ height: "100px", width: "100px" }}></img>
+                        <div style={{ fontSize: "25px" }}>0+ Bubbles</div>
+                    </div>
+
+                    <div style={{ textAlign: "center" }}>
+                        <img src={User} style={{ height: "100px", width: "100px" }}></img>
+                        <div style={{ fontSize: "25px" }}>0+ Users</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="welcome-section">
+                <div className="section-title">
+                    <div className="section-title-bullet" style={{ backgroundColor: "#D3B99F" }}></div>
+                    <span className="section-title-text">Dedicated to Your Delight</span>
+                </div>
+                <div className="section-content" style={{ backgroundColor: "#D3B99F", minHeight: "400px", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+
+                    <TestimonialCard/>
+                    <TestimonialCard/>
+                </div>
+            </div>
+
 
         </>
     );
@@ -40,6 +82,17 @@ function HeroBox() {
             </div>
 
         </>
+    )
+}
+
+function TestimonialCard() {
+    return (
+        <div style={{ textAlign: "center", width:"450px", border:"solid", paddingBlock:"10px" }}>
+            <img src={Avatar} style={{ height: "200px", width: "200px" }}></img>
+            <div style={{ fontSize: "25px" }}>“Absolutely the best service I've ever used!
+             It's incredibly user-friendly and impressively fast.
+              The overall user experience is nothing short of fantastic”</div>
+        </div>
     )
 }
 
